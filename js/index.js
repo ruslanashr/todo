4d0f4e4cc2eeed8452a62352645e75e1b0a36059
Ruslana <ruslanakyvenko@ukr.net>
2019-05-25 17:18:38 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,47 @@
+const add = document.querySelector('.add');
+const remove = document.querySelector('.btn-danger');
+var col_todo = document.querySelector('.col-todo');
+const input = document.querySelector('.form-control')
+
+function addtodo(el) {
+    const text = `<div class="input-group mb-3 todo">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <input type="checkbox" aria-label="Checkbox for following text input"></input>
+                        </div>
+                    </div>
+                        <span>${el}</span>
+                        <button type="button" class="btn btn-danger">X</button>
+                </div>`
+    const position = "beforeend";
+    col_todo.insertAdjacentHTML(position, text);
+}
+document.addEventListener("keyup",function(event){
+    if(event.keyCode == 13){
+        const el = input.value;
+        if(el){
+            addtodo(el);
+        }
+        input.value = "";
+    }
+})
+document.addEventListener("click", function(event){
+    let button = event.target;
+    const buttonclass = button.classList.contains('add');
+    const el = input.value;
+    if(buttonclass){
+        addtodo(el);
+        input.value = "";
+    }
+})
+
+function removetodo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+}
+col_todo.addEventListener("click", function(event){
+    let element = event.target;
+    const elementclass = element.classList.contains('btn-danger');
+    if(elementclass){
+        removetodo(element);
+    }
+})
